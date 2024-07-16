a2460c48289ff9cd4df7c6fb06b38cefe2c44d45
tingxiecsu <81673071+tingxiecsu@users.noreply.github.com>
2024-07-16 17:04:41 +0800

    Update train.py

--- ConSS/train.py
+++ ConSS/train.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 27 14:24:01 2022
 
-@author: ZNDX002
-"""
 import torch
-
-# from models.resnet_clr import ResNetSimCLR
-from model import ConSS
+import torch.distributed as dist
+from model import ModelCLR
 from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as F
 from loss.nt_xent import NTXentLoss
@@ -18,6 +12,7 @@ import sys
 from tqdm import tqdm
 from transformers import AdamW
 import logging
+from torch.nn.parallel import DistributedDataParallel
 logging.getLogger("transformers.tokenization_utils_base").setLevel(logging.ERROR)
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -41,9 +36,11 @@ def _save_config_file(model_checkpoints_folder):
         shutil.copy('./config.yaml', os.path.join(model_checkpoints_folder, 'config.yaml'))
 
 class SimCLR(object):
-    def __init__(self, dataset, config):
+    def __init__(self, dataset, config,device,world_size,rank):
         self.config = config
-        self.device = self._get_device()
+        self.device = device
+        self.world_size = world_size
+        self.rank = rank
         self.writer = SummaryWriter()
         self.dataset = dataset
         self.nt_xent_criterion = NTXentLoss(self.device, config['batch_size'], **config['loss'])
@@ -57,13 +54,16 @@ class SimCLR(object):
         #Dataloaders
         train_loader, valid_loader = self.dataset.get_data_loaders()
 
-        #Model Resnet Initialize
-        model = ConSS(**self.config["model_config"]).to(self.device)
+        model = ModelCLR(**self.config["model_config"]).to(self.device)
         if self.config["smilesmodel_finetune"]:
             state_dict = torch.load(self.config["smilesmodel_finetune_path"], map_location=self.device)
             model.Smiles_model.load_state_dict(state_dict)
         else:
             model = self._load_pre_trained_weights(model)
+
+        if self.world_size > 1:
+            model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model).to(self.rank) 
+            model = DistributedDataParallel(model, device_ids=[self.rank], find_unused_parameters = True)
 
         optimizer = torch.optim.Adam(model.parameters(), 
                                         eval(self.config['learning_rate']), 
@@ -86,7 +86,8 @@ class SimCLR(object):
         model_checkpoints_folder = os.path.join(self.writer.log_dir, 'checkpoints')
 
         # save config file
-        _save_config_file(model_checkpoints_folder)
+        if dist.get_rank() == 0:
+            _save_config_file(model_checkpoints_folder)
 
         n_iter = 0
         valid_n_iter = 0
@@ -95,22 +96,23 @@ class SimCLR(object):
         print(f'Training...')
 
         for epoch_counter in range(self.config['epochs']):
+            train_loader.sampler.set_epoch(epoch_counter)
             print(f'Epoch {epoch_counter}')
-            for xis, xls  in tqdm(train_loader):
-                train_loss = 0
+            train_loss = 0
+            for (xis, mzs,intens,num_peaks)  in tqdm(train_loader):
                 optimizer.zero_grad()
-
                 xis = xis.to(self.device)
-                xls = torch.stack([s for s in xls])
-                xls = xls.to(self.device)
-
+                mzs = mzs.to(self.device)
+                intens = intens.to(self.device)
+                num_peaks = num_peaks.to(self.device)
+                
                 # get the representations and the projections
-                zis, zls = model(xis, xls)  # [N,C]
+                zis, zls = model(xis,mzs,intens,num_peaks)  # [N,C]
 
                 loss = self.nt_xent_criterion(zis, zls)
-
-                if n_iter % self.config['log_every_n_steps'] == 0:
-                    self.writer.add_scalar('train_loss', loss, global_step=n_iter)
+                if dist.get_rank() == 0:
+                    if n_iter % self.config['log_every_n_steps'] == 0:
+                        self.writer.add_scalar('train_loss', loss, global_step=n_iter)
 
                 if apex_support and self.config['fp16_precision']:
                     with amp.scale_loss(loss, optimizer) as scaled_loss:
@@ -121,22 +123,30 @@ class SimCLR(object):
                 optimizer.step()
                 train_loss += loss.item()
                 n_iter += 1
-            print('Training at Epoch ' + str(epoch_counter + 1) + 'with loss ' + str(train_loss))
+            if dist.get_rank() == 0:
+                print('Training at Epoch ' + str(epoch_counter + 1) + 'with loss ' + str(train_loss))
             # validate the model if requested
             if epoch_counter % self.config['eval_every_n_epochs'] == 0:
                 valid_loss = self._validate(model, valid_loader, n_iter)
-                print('Validation at Epoch ' + str(epoch_counter + 1) + 'with loss ' + str(valid_loss))
+                if dist.get_rank() == 0:
+                    print('Validation at Epoch ' + str(epoch_counter + 1) + 'with loss ' + str(valid_loss))
                 if valid_loss < best_valid_loss:
                     # save the model weights
                     best_valid_loss = valid_loss
-                    torch.save(model.state_dict(), os.path.join(model_checkpoints_folder, 'model.pth'))
-                self.writer.add_scalar('validation_loss', valid_loss, global_step=valid_n_iter)
+                    if dist.get_rank() == 0:
+                        #torch.save(net.module, "saved_model.ckpt") 
+                        torch.save(model.module.state_dict(), os.path.join(model_checkpoints_folder, 'model.pth'))
+                if dist.get_rank() == 0:
+                    self.writer.add_scalar('validation_loss', valid_loss, global_step=valid_n_iter)
                 valid_n_iter += 1
 
             # warmup for the first 10 epochs
             if epoch_counter >= 10:
                 scheduler.step()
             self.writer.add_scalar('cosine_lr_decay', scheduler.get_lr()[0], global_step=n_iter)
+
+        if self.world_size > 1:
+            dist.destroy_process_group()
 
     def _load_pre_trained_weights(self, model):
         try:
@@ -157,14 +167,15 @@ class SimCLR(object):
             valid_loss = 0.0
             counter = 0
             print(f'Validation step')
-            for xis, xls  in tqdm(valid_loader):
+            for (xis,mzs,intens,num_peaks)  in tqdm(valid_loader):
 
                 xis = xis.to(self.device)
-                xls = torch.stack([s for s in xls])
-                xls = xls.to(self.device)
+                mzs = mzs.to(self.device)
+                intens = intens.to(self.device)
+                num_peaks = num_peaks.to(self.device)
 
                 # get the representations and the projections
-                zis, zls = model(xis, xls)  # [N,C]
+                zis, zls = model(xis,mzs,intens,num_peaks)  # [N,C]
 
                 loss = self.nt_xent_criterion(zis, zls)
 
